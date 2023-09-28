f1715e23705775c8a8b468ccf21f260857eef58e
Yii Kuo Chong <byu1628@proton.me>
2023-09-29 00:01:16 +0800

    Removed unused import in TinySidebar

--- src/components/TinySidebar/constants.tsx
+++ src/components/TinySidebar/constants.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BsFileEarmarkText } from 'react-icons/bs'
-import TinySidebarIcon from '../TinySidebarIcon/component'
 
 const TINY_SIDEBAR_NAVIGATION_ITEMS = [
     {
