938afd877b359562c2f42d09a2f930bc46885280
Yii Kuo Chong <byu1628@proton.me>
2024-01-13 01:25:51 +0800

    feat: add statusbar

---
+++ src/components/Status/component.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Status = () => (
+    <div className="h-5 bg-zinc-900 border-solid border-t border-t-zinc-700">
+
+    </div>
+)
+
+export default Status;
---
+++ src/components/Status/index.ts
@@ -0,0 +1,3 @@
+import Status from './component';
+
+export default Status;
