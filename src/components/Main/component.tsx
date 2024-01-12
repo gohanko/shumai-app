3f21988d2a4494059324db3f54d6a0f0d3f2651e
Yii Kuo Chong <byu1628@proton.me>
2024-01-12 19:08:57 +0800

    feat: added UI for the main container and the sidebar navigation

---
+++ src/components/Main/component.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Sidebar from "../Sidebar/component";
+
+const Main = () => (
+    <div className="w-full flex-1">
+        <Sidebar />
+    </div>
+)
+
+export default Main
--- src/components/Sidebar/component.tsx
+++ src/components/Sidebar/component.tsx
@@ -1,17 +1,19 @@
 import React from "react";
-import ActivityBarIcon from "../SidebarIcon/component";
-import { TINY_SIDEBAR_NAVIGATION_ITEMS } from "./constants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCubes, faLaptopCode, faPuzzlePiece, faGear } from "@fortawesome/free-solid-svg-icons";
+import IconButton from "../IconButton";
 
-const ActivityBar = () => {
-    return (
-        <div className="flex-1 flex flex-col h-screen w-10 m-0">
-            <div className={'flex h-10 bg-zinc-700'}>
-            </div>
-            <div className={'flex-1 bg-zinc-800 border-r border-solid border-zinc-700'}>
-                { TINY_SIDEBAR_NAVIGATION_ITEMS.map((item) => <ActivityBarIcon icon={item.icon} tooltip_label={item.tooltip_label} />) }
-            </div>
+const Sidebar = () => (
+    <div className="w-10 h-full flex flex-col bg-zinc-900 border-solid border-r-2 border-zinc-700">
+        <div className="flex-1">
+            <IconButton item={<FontAwesomeIcon icon={faCubes} />} />
+            <IconButton item={<FontAwesomeIcon icon={faLaptopCode} />} />
+            <IconButton item={<FontAwesomeIcon icon={faPuzzlePiece} />} />
         </div>
-    )
-}
+        <div className="flex-1 flex items-end">
+            <IconButton item={<FontAwesomeIcon icon={faGear} />} />
+        </div>
+    </div>
+)
 
-export default ActivityBar;
+export default Sidebar;
