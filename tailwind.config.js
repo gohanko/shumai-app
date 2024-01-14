580be6264d66d7ca9da1924190aa7ef8d0ab5a21
Yii Kuo Chong <byu1628@proton.me>
2024-01-15 01:24:24 +0800

    chore: modify outdated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     mode: 'jit',
-    darkMode: false,
-    purge: [
-        './src/**/*.{js,jsx,ts,tsx}',
-        './public/index.html'
-    ],
     content: [
         './src/**/*.{js,jsx,ts,tsx}',
     ],
