f2b95e6b57f34b1cafc0165ee6fb54a14f84a7c1
Yii Kuo Chong <byu1628@proton.me>
2024-01-19 04:20:52 +0800

    feat: implement hoverlay scrollbar

--- src/ui/List/list.tsx
+++ src/ui/List/list.tsx
@@ -8,7 +8,7 @@ type ListProps = {
 const List = ({ children, onClick}: ListProps) => (
     <div className='grow min-h-0' onClick={onClick}>
         <div
-            className="grow h-0 min-h-full border-solid border-t border-zinc-700 overflow-y-scroll overflow-x-hidden scroll-smooth  "
+            className="grow h-0 min-h-full border-solid border-t border-zinc-700 overflow-y-scroll overflow-x-hidden scrollbar-thumb-rounded-full   scrollbar-thumb-zinc-700 scrollbar-thin  "
             onClick={onClick}
         >
             { children }
--- src/ui/Tabbing/tab_list.tsx
+++ src/ui/Tabbing/tab_list.tsx
@@ -1,14 +1,30 @@
 import React from "react"
+import { OverlayScrollbarsComponent } from "overlayscrollbars-react";
 
 type TabListProps = {
     children: React.ReactNode
 }
 
 const TabList = ({ children }: TabListProps) => (
-    <div className="flex-none grow min-w-0 h-10">
-        <div className="grow w-0 min-w-full overflow-x-scroll flex">
-            { children }
-        </div>
+    <div className="flex-none grow min-w-0 h-10 scrollbar-thumb-zinc-700 scrollbar-thin">
+        <OverlayScrollbarsComponent
+            element="span"
+            options={{
+                scrollbars: {
+                    theme: 'scrollbar-custom-theme',
+                    autoHide: 'leave',
+                    autoHideDelay: 1300,
+                    autoHideSuspend: false,
+                    dragScroll: true,
+                    clickScroll: false,
+                } 
+            }}
+            defer
+        >
+                <div className="flex-none grow h-10 w-0 min-w-full flex">
+                    { children }
+                </div>
+        </OverlayScrollbarsComponent>
     </div>
 )
 
